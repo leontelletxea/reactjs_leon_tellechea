884a76eac3f5673a6741246c3d13620bf6d9bd6c
leontelletxea <leontelletxea@gmail.com>
2022-04-27 19:37:56 -0300

    catalogo con map y promise

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,17 +1,17 @@
-import { getActiveElement } from '@testing-library/user-event/dist/utils'
 import React from 'react'
 import './Item.css'
 import ItemCount from '../ItemCount/ItemCount';
 
 const Item = ({item}) => {
   return (
-    <div class="card d-flex">
+    <div class="card">
     <div class="card-body">
-      <img class="card-img-top" src={item.image}/>
-      <h1 class="card-title text-dark">{item.title}</h1>
-      <p class="card-text text-dark">{item.description}</p>
-      <h5 class="card-title text-dark">AR${item.price}</h5>
-      <div class="count"><ItemCount stock={item.stock} initial={0} onAdd={handlerAddToCart}/></div>
+      <div class="imgContainer">
+      <img class="img" src={item?.image} alt={item.image}/>
+      </div>
+      <h1 class="card-title text-dark">{item?.title}</h1>
+      <h5 class="card-title text-dark">AR${item?.price}</h5>
+      <div class="count"><ItemCount stock={item?.stock} initial={0} onAdd={handlerAddToCart}/></div>
     </div>
     </div>
   )
@@ -22,3 +22,4 @@ function handlerAddToCart(count){
 }
 
 export default Item
+
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import LoadProds from './LoadProds'
+import React, { useEffect, useState } from 'react'
+import ItemList from '../ItemList/ItemList';
 import './ItemListContainer.css'
 
 const ItemListContainer = () => {
@@ -7,6 +7,61 @@ const ItemListContainer = () => {
   return (
     <div class="box">
       <LoadProds/>
+    </div>
+  )
+}
+
+const LoadProds = () => {
+
+  const [catalogo, setJuegos] = useState([]);
+
+  function obtainProds() {
+    const myPromise = new Promise((resolve) => {
+      const listado = [
+        {
+          id: 1,
+          title: "Days Gone",
+          image: "https://as.com/meristation/imagenes/2020/11/03/game_cover/666031971604438123.jpg",
+          price: 11200,
+          stock: 5
+        },
+        {
+          id: 2,
+          title: "God Of War",
+          image: "https://s3.gaming-cdn.com/images/products/7325/orig-fallback-v1/god-of-war-pc-juego-steam-cover.jpg",
+          price: 9500,
+          stock: 2
+        },
+        {
+          id: 3,
+          title: "Gta V",
+          image: "https://media.vandal.net/m/15192/grand-theft-auto-v-2015413122229_1.jpg",
+          price: 5000,
+          stock: 7
+        },
+        {
+          id: 4,
+          title: "Fifa 22",
+          image: "https://img.g2a.com/323x433/1x1x0/fifa-22-pc-origin-key-europe/dce7598de0604b70ae47d576",
+          price: 13900,
+          stock: 3
+        }
+      ]
+      setTimeout(() => {
+        resolve(listado);
+      }, 2000);
+    });
+    return myPromise;
+  }  
+
+  useEffect(() => {
+    obtainProds()
+      .then(response => setJuegos(response)); // Ejecuta la respuesta de la promise cuando se cumple la promesa
+  }, [])
+
+  return (
+    <div>
+      <ItemList items={catalogo}/>
     </div>
   )
 }
